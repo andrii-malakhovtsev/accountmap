b8be4d7295a6e226d5852f2cb2c879eca47db00d
wens-kedar <wenskedar.ba@gmail.com>
2026-02-07 00:29:56 -0500

    successful api requst

--- backend/src/routes/account.route.ts
+++ backend/src/routes/account.route.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { prisma } from "../lib/prisma";
+import { get_default_user } from "../lib/utils";
 
 const router = express.Router();
 
 router.route("/bulk").post(async (req: any, res: any) => {
 	try {
+		const userid = (await get_default_user())!.id;
 		const createMany = await prisma.account.createMany({
-			data: req.body,
+			data: { ...req.body[0], userid: userid },
 		});
 		res.status(200).json({ message: `Successfully created ${createMany} users.` });
 	} catch (error) {
