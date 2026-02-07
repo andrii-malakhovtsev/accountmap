57e9f5cc983b0e70b0e730eef9ffb4dd84530a48
wens-kedar <wenskedar.ba@gmail.com>
2026-02-07 04:53:36 -0500

    identity apis done

--- backend/src/routes/index.route.ts
+++ backend/src/routes/index.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import accountsRoutes from "./account.route";
 import connectionsRoutes from "./connection.route";
+import identitiesRoutes from "./identity.route";
 
 const router = express.Router();
 
 router.use("/accounts", accountsRoutes);
 router.use("/connections", connectionsRoutes);
-
+router.use("/identities", identitiesRoutes);
 export default router;
