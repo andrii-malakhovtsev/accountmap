b8c2156f41b99ab44133e9642043d4caca1e56b6
wens-kedar <wenskedar.ba@gmail.com>
2026-02-07 00:09:22 -0500

    conncectio established

--- backend/src/routes/account.route.ts
+++ backend/src/routes/account.route.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import { prisma } from "../lib/prisma";
 
 const router = express.Router();
 
-router.route("/bulk").post((req: any, res: any) => {
-	console.log("api is here");
+router.route("/bulk").post(async (req: any, res: any) => {
+	try {
+		const createMany = await prisma.account.createMany({
+			data: req.body,
+		});
+		res.status(200).json({ message: `Successfully created ${createMany} users.` });
+	} catch (error) {
+		console.error("Error creating users:", error);
+		res.status(500).json({ error: "An error occurred while creating users." });
+	}
 });
 
 export default router;
